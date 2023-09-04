650f53d814203a64372f2295d682166114fb4efc
rubenparnell <66206505+rubenparnell@users.noreply.github.com>
2023-09-04 17:01:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Function to check if a date is a weekend (Saturday or Sunday)
-function isWeekend(date) {
-    const day = date.getDay();
-    return day === 0 || day === 6;
-}
-
 // Function to check if a date should be skipped based on a list of skipDates
 function shouldSkipDate(date, skipDates) {
     const dateString = date.toISOString().split('T')[0]; // Get YYYY-MM-DD
@@ -15,19 +9,14 @@ function updateCountdown(targetTimes, skipDates) {
     const now = new Date();
     const currentTime = now.getTime();
 
-    // Find the closest target time on weekdays
+    // Find the closest target time
     let closestTime = null;
-    let foundToday = false;
 
     for (const time of targetTimes) {
         const targetTime = new Date(now);
         targetTime.setHours(time.hours, time.minutes, 0, 0);
 
-        // Skip weekends
-        if (isWeekend(targetTime)) {
-            continue;
-        }
-
+        // Skip specific dates
         if (shouldSkipDate(targetTime, skipDates)) {
             continue;
         }
@@ -37,31 +26,16 @@ function updateCountdown(targetTimes, skipDates) {
             targetTime.setDate(targetTime.getDate() + 1);
         }
 
-        if (targetTime > now && (closestTime === null || targetTime < closestTime)) {
+        if (closestTime === null || targetTime < closestTime) {
             closestTime = targetTime;
-            foundToday = true;
         }
-    }
-
-    if (!foundToday && closestTime === null) {
-        // If there are no more bells on weekdays today and none scheduled for the future,
-        // find the first event on the next weekday
-        let nextDay = new Date(now);
-        while (true) {
-            nextDay.setDate(nextDay.getDate() + 1);
-            if (!isWeekend(nextDay) && !shouldSkipDate(nextDay, skipDates)) {
-                break;
-            }
-        }
-        closestTime = new Date(nextDay);
-        closestTime.setHours(targetTimes[0].hours, targetTimes[0].minutes, 0, 0);
     }
 
     const timeRemaining = closestTime - currentTime;
 
     if (timeRemaining <= 0) {
-        // If all target times have passed, you can handle it here.
-        document.getElementById("countdown-timer").innerHTML = "All weekday events have ended";
+        // If all target times have passed or all are on skipped days, you can handle it here.
+        document.getElementById("countdown-timer").innerHTML = "No upcoming events";
         return;
     }
 
@@ -81,6 +55,20 @@ function updateCountdown(targetTimes, skipDates) {
     document.getElementById("next-event-time").innerText = nextEventTime;
 }
 
+// Define an array of target times
+const targetTimes = [
+    { hours: 8, minutes: 45 },
+    { hours: 9, minutes: 10 },
+    { hours: 10, minutes: 10 },
+    { hours: 11, minutes: 10 },
+    { hours: 11, minutes: 30 },
+    { hours: 12, minutes: 30 },
+    { hours: 13, minutes: 15 },
+    { hours: 14, minutes: 15 },
+    { hours: 15, minutes: 15 },
+    { hours: 17, minutes: 15 },
+    { hours: 18, minutes: 15 }
+];
 
 // Define an array of dates to skip (in YYYY-MM-DD format)
 const skipDates = ["2023-09-07", "2023-09-08"]; // Add dates to be skipped here
