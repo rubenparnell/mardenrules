5e3a323b5010f42e4e60980e0dae9d179f0627dc
rubenparnell <66206505+rubenparnell@users.noreply.github.com>
2023-09-04 17:09:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Function to check if a date is a weekend (Saturday or Sunday)
+function isWeekend(date) {
+    const day = date.getDay();
+    return day === 0 || day === 6;
+}
+
 // Function to check if a date should be skipped based on a list of skipDates
 function shouldSkipDate(date, skipDates) {
     const dateString = date.toISOString().split('T')[0]; // Get YYYY-MM-DD
@@ -9,14 +15,19 @@ function updateCountdown(targetTimes, skipDates) {
     const now = new Date();
     const currentTime = now.getTime();
 
-    // Find the closest target time
+    // Find the closest target time on weekdays
     let closestTime = null;
+    let foundToday = false;
 
     for (const time of targetTimes) {
         const targetTime = new Date(now);
         targetTime.setHours(time.hours, time.minutes, 0, 0);
 
-        // Skip specific dates
+        // Skip weekends
+        if (isWeekend(targetTime)) {
+            continue;
+        }
+
         if (shouldSkipDate(targetTime, skipDates)) {
             continue;
         }
@@ -26,9 +37,24 @@ function updateCountdown(targetTimes, skipDates) {
             targetTime.setDate(targetTime.getDate() + 1);
         }
 
-        if (closestTime === null || targetTime < closestTime) {
+        if (targetTime > now && (closestTime === null || targetTime < closestTime)) {
             closestTime = targetTime;
+            foundToday = true;
         }
+    }
+
+    if (!foundToday && closestTime === null) {
+        // If there are no more bells on weekdays today and none scheduled for the future,
+        // find the first event on the next weekday
+        let nextDay = new Date(now);
+        while (true) {
+            nextDay.setDate(nextDay.getDate() + 1);
+            if (!isWeekend(nextDay) && !shouldSkipDate(nextDay, skipDates)) {
+                break;
+            }
+        }
+        closestTime = new Date(nextDay);
+        closestTime.setHours(targetTimes[0].hours, targetTimes[0].minutes, 0, 0);
     }
 
     const timeRemaining = closestTime - currentTime;
